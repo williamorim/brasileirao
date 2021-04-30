16ac0d868cf81df03f2a09ea3df28132b5ed06d7
williamorim <thewilliam89@gmail.com>
2021-04-30 19:37:23 -0300

    adding 2021 matches

--- data-raw/update_matches.R
+++ data-raw/update_matches.R
@@ -6,8 +6,8 @@ source("data-raw/scraping_matches.R")
 matches_2021 <- purrr::map_dfr(1:38, scraper_ge_2021)
 
 matches <- matches %>%
-  dplyr::filter(season != 2020) %>%
-  dplyr::bind_rows(matches_2020)
+  dplyr::filter(season != 2021) %>%
+  dplyr::bind_rows(matches_2021)
 
 readr::write_csv(matches, "data-raw/csv/matches.csv")
 
