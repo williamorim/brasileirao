c96b8c6037ffa12e10683e7fd0ec4ab0c978c63d
williamorim <thewilliam89@gmail.com>
2022-09-16 19:41:14 -0300

    colocando a coluna id na tabela matches

--- data-raw/update_matches.R
+++ data-raw/update_matches.R
@@ -19,10 +19,25 @@ source("data-raw/scraping_matches.R")
 
 matches_2022 <- purrr::map_dfr(1:38, scraper_ge_2022)
 
-matches <- matches %>%
-  dplyr::filter(season != 2022) %>%
-  dplyr::bind_rows(matches_2022) |>
-  dplyr::mutate(dplyr::across(c(home, away), fix_names))
+matches_2022 <- matches_2022 |>
+  dplyr::mutate(
+    dplyr::across(c(home, away), fix_names)
+  )
+
+matches <- matches |>
+  dplyr::left_join(
+    matches_2022,
+    by = c("season", "home", "away"),
+    suffix = c("", "_new")
+  ) |>
+  dplyr::mutate(
+    date = dplyr::case_when(
+      !is.na(date_new) ~ date_new,
+      TRUE ~ date
+    ),
+    score = ifelse(!is.na(score_new), score_new, score)
+  ) |>
+  dplyr::select(-dplyr::ends_with("new"))
 
 readr::write_csv(matches, "data-raw/csv/matches.csv")
 
