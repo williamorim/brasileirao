d52b882543a742d4bb8a5c679930aaa48229e1e7
williamorim <thewilliam89@gmail.com>
2023-05-19 12:32:48 -0300

    2023 matches

--- data-raw/matches.R
+++ data-raw/matches.R
@@ -1,11 +1,16 @@
 ## code to prepare `matches` dataset goes here
 
 devtools::load_all()
+source("data-raw/scraping_matches.R")
+
+matches_2023 <- purrr::map_dfr(1:38, scraper_ge_2023)
+
+new_id <- max(matches$id_match) + 1
+
+matches_2023 <- matches_2023 |>
+  dplyr::mutate(id_match = new_id:(new_id + dplyr::n() - 1))
 
 matches <- matches |>
-  dplyr::mutate(
-    id_match = dplyr::row_number()
-  ) |>
-  dplyr::relocate(id_match)
+  dplyr::bind_rows(matches_2023)
 
 usethis::use_data(matches, overwrite = TRUE)
