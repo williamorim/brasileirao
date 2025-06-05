20db237b48e002a62be06344520eb1da1a3f9910
williamorim <thewilliam89@gmail.com>
2025-06-05 18:54:32 -0300

    arrumando dados de 2025

--- data-raw/matches.R
+++ data-raw/matches.R
@@ -8,7 +8,10 @@ matches_2025 <- purrr::map_dfr(1:38, scraper_ge_2025)
 new_id <- max(matches$id_match) + 1
 
 matches_2025 <- matches_2025 |>
-  dplyr::mutate(id_match = new_id:(new_id + dplyr::n() - 1))
+  dplyr::mutate(
+    id_match = new_id:(new_id + dplyr::n() - 1),
+    dplyr::across(c(home, away), fix_names)
+  )
 
 matches <- matches |>
   dplyr::bind_rows(matches_2025)
